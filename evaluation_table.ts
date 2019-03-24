7adc53abdbf4b6b890085b2f1efdfbc005581e2f
Kozo Oeda <kozo.oeda@gmail.com>
2019-03-24 14:32:29 +0900

    Add evaluation table codes

---
+++ evaluation_table.ts
@@ -0,0 +1,76 @@
+type SourceEvaluation = "N" | "A" | "B" | "C" | "X";
+type InfoEvaluation = 0 | 1 | 2 | 3 | 4;
+type SourceReliability = "N" | "A" | "B" | "C" | "D" | "E" | "F";
+type DataValidity = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+type Language = "EN" | "JP";
+
+interface Mat4x4 {
+    source_rank: SourceEvaluation;
+    info_rank: InfoEvaluation;
+}
+
+interface Detail4x4 {
+    source_detail: Array < string > ;
+    info_detail: Array < string > ;
+}
+
+class EvaluationTable implements Mat4x4 {
+    constructor(public source_rank: SourceEvaluation = "N", public info_rank: InfoEvaluation = 0, private lang: Language = "EN") {}
+
+    detail_source = (): Array < string > => {
+        switch (this.lang) {
+            case "EN":
+                switch (this.source_rank) {
+                    case "N":
+                        const det_n0: string = "Not evaluated yet.";
+                        return [det_n0]
+                    case "A":
+                        const det_a0: string = "No doubt regarding authenticity, trustworthiness, integrity, competence.";
+                        const det_a1: string = "History of complete reliability.";
+                        return [det_a0, det_a1]
+                    case "B":
+                        const det_b0: string = "Source from whom information received has in most instances proved to be reliable.";
+                        return [det_b0]
+                    case "C":
+                        const det_c0: string = "Source from whom information received has in most instances proved to be unreliable.";
+                        return [det_c0]
+                    case "X":
+                        const det_x0: string = "Reliability cannot be judged.";
+                        return [det_x0]
+                }
+        }
+    }
+
+    detail_info = (): Array < string > => {
+        switch (this.lang) {
+            case "EN":
+                switch (this.info_rank) {
+                    case 0:
+                        const det_00: string = "Not evaluated yet.";
+                        return [det_00]
+                    case 1:
+                        const det_10: string = "No doubt about accuracy.";
+                        return [det_10]
+                    case 2:
+                        const det_20: string = "Information known personally to the source but not known personally to the official who is passing it on";
+                        const det_21: string = "Logical in itself.";
+                        const det_22: string = "Agrees with other information on the subject.";
+                        return [det_20, det_21, det_22]
+                    case 3:
+                        const det_30: string = "Information not known personally to the source but corroborated by other information already recorded.";
+                        return [det_30]
+                    case 4:
+                        const det_40: string = "nformation which is not known personally to the source and can not be independently corroborated.";
+                        return [det_40]
+                }
+        }
+    }
+
+    detail = (): Detail4x4 => {
+        const det4x4 = {
+            source_detail: this.detail_source(),
+            info_detail: this.detail_info()
+        }
+        return det4x4
+    }
+}
