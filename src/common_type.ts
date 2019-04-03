5e53b6f5f9846a2799aa9da81e70d3ba7d79e4ec
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-03 22:39:45 +0900

    Fix

--- src/common_type.ts
+++ src/common_type.ts
@@ -6,7 +6,7 @@ export type Data = SubjectData | ObjectData | LinkData | unknown;
 export type DataType = "subject" | "object" | "link5x5" | "link7x7";
 
 export interface Time {
-    readonly created:
-    readonly updated:
-    readonly deleted:
+    readonly created;
+    readonly updated;
+    readonly deleted;
 }
