2f5df8523db31534319bc443b7ae0950c3602aad
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-05 19:33:52 +0900

    Change tab spaces

--- src/evaluation_table/evaluation_table_base.ts
+++ src/evaluation_table/evaluation_table_base.ts
@@ -1,12 +1,12 @@
 type table_type = "5x5" | "7x7";
 
 export interface Detail {
-    source_detail: Array < string >;
-    info_detail: Array < string >;
+  source_detail: Array < string >;
+  info_detail: Array < string >;
 }
 
 export interface Mat<T, U> {
-    detail_source(source_rank: T): Array < string >;
-    detail_info(info_rank: U): Array < string >;
-    detail(): Detail;
+  detail_source(source_rank: T): Array < string >;
+  detail_info(info_rank: U): Array < string >;
+  detail(): Detail;
 } 
--- src/medium_arrow/medium_arrow.ts
+++ src/medium_arrow/medium_arrow.ts
@@ -5,62 +5,62 @@ import { Rank7x7, SourceReliability, DataValidity } from "../evaluation_table/ev
 // Ex: A => B (A phoned B)
 
 interface MediumArrow {
-    readonly arrow_id: DataID;
-    readonly core_sentence: string;
-    src_x: number;
-    src_y: number;
-    dst_x: number;
-    dst_y: number;
+  readonly arrow_id: DataID;
+  readonly core_sentence: string;
+  src_x: number;
+  src_y: number;
+  dst_x: number;
+  dst_y: number;
 }
 
 export interface SubjectData {
-    readonly subject_id: DataID;
-    time: Time;
+  readonly subject_id: DataID;
+  time: Time;
 }
 
 interface SubjectData5x5 extends Rank5x5 {
-    data: SubjectData;
+  data: SubjectData;
 }
 
 interface SubjectData7x7 extends Rank7x7 {
-    data: SubjectData;
+  data: SubjectData;
 }
 
 export interface ObjectData {
-    readonly object_id: DataID;
-    time: Time;
+  readonly object_id: DataID;
+  time: Time;
 }
 
 interface ObjectData5x5 extends Rank5x5 {
-    data: ObjectData;
+  data: ObjectData;
 } 
 
 interface ObjectData7x7 extends Rank7x7 {
-    data: ObjectData;
+  data: ObjectData;
 } 
 
 export interface LinkData {
-    readonly link_id: DataID;
-    time: Time;
+  readonly link_id: DataID;
+  time: Time;
 }
 
 interface LinkData5x5 extends Rank5x5 {
-    data: LinkData;
+  data: LinkData;
 }
 
 interface LinkData7x7 extends Rank7x7 {
-    data: LinkData;
+  data: LinkData;
 }
 
 class Link implements SubjectData, ObjectData, MediumArrow {
-    constructor (public link_id: DataID, public subject_id: DataID, public object_id: DataID, public arrow_id: DataID, public core_sentence: string, public src_x: number, public src_y: number, public dst_x: number, public dst_y: number, public time: Time){}
+  constructor (public link_id: DataID, public subject_id: DataID, public object_id: DataID, public arrow_id: DataID, public core_sentence: string, public src_x: number, public src_y: number, public dst_x: number, public dst_y: number, public time: Time){}
 
-    subject_data = (): Data => {
-        return    
-    }
+  subject_data = (): Data => {
+    return    
+  }
 
-    object_data = (): Data => {
-        return    
-    }
+  object_data = (): Data => {
+    return    
+  }
 }
 
