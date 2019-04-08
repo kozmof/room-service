b60b5fdd6370661f48dd5c6f77f3d1b9d3af074d
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-08 23:14:52 +0900

    Add ui directory

---
+++ src/ui/Stack.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+
+interface CallPack<T> {
+  callPack(): (item: T) => JSX.Element;
+}
+
+export interface StackItems<T> {
+  items: Array<T>;
+}
+
+export abstract class IMStack<T, P extends StackItems<T>, S> extends React.Component<P, S> implements CallPack<T> {
+  constructor(props: P, initial_state: S){
+    super(props); 
+    this.state = initial_state;
+    this.callPack.bind(this)
+  }
+
+  abstract callPack(): (item: T) => JSX.Element;
+
+  render(){
+    const items = this.props.items || [];
+    const rows = items.map(this.callPack());
+    return(
+      <table> 
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export abstract class MStack<T, P, S extends StackItems<T>> extends React.Component<P, S> implements CallPack<T> {
+  constructor(props: P, initial_state: S){
+    super(props); 
+    this.state = initial_state;
+    this.callPack.bind(this);
+  }
+
+  abstract callPack(): (item: T) => JSX.Element;
+
+  render(){
+    const rows = this.state.items.map(this.callPack());
+    return(
+      <table> 
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
