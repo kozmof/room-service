99d7834d52227a0d23560b20f31dfa811adb8b3a
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-09 20:30:21 +0900

    Add data surfaces

--- src/common_type.ts
+++ src/common_type.ts
@@ -4,9 +4,35 @@ export type DataID = string;
 export type UserID = string;
 export type Data = SubjectData | ObjectData | LinkData | unknown;
 export type DataType = "subject" | "object" | "link5x5" | "link7x7";
+export type UserAction = "created" | "modified" | "deleted";
+export type ModifiedDataType = string;
 
 export interface Time {
   readonly created;
   readonly updated;
   readonly deleted;
 }
+
+export interface CoreDataSurface {
+  data_id: DataID;
+  source: string;
+  MD5: string;
+  author_name: string;
+  author_id: UserID;
+  editor_name: string;
+  editor_id: UserID;
+}
+
+export interface LogDataSurface extends CoreDataSurface {
+  user_action: DataType;
+  modified_data: ModifiedDataType;
+  time: Date;
+}
+
+export interface CommonDataSurface extends CoreDataSurface {
+  memo: string;
+  category: Array<string>;
+  change_log: Array<LogDataSurface>;
+  tag: Array<string>
+}
+
