e6832528a803e8167ea796c45b8842cc24d97d18
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-16 21:10:28 +0900

    Update a test editor

--- src/editor/TestEditor.tsx
+++ src/editor/TestEditor.tsx
@@ -13,6 +13,22 @@ export class TestEditor extends React.Component<{}, TestState> {
   }
 
   handleChange(e: EditorState) {
+    const selectionState = e.getSelection();
+    const anchorKey = selectionState.getAnchorKey();
+
+    const currentContent = e.getCurrentContent();
+    const currentContentBlock = currentContent.getBlockForKey(anchorKey);
+
+    const start = selectionState.getStartOffset();
+    const end = selectionState.getEndOffset();
+
+    const selectedText = currentContentBlock.getText().slice(start, end);
+
+    console.log("--")
+    console.log(anchorKey);
+    console.log(start);
+    console.log(end);
+    console.log(selectedText)
     this.setState({ editorState: e });
   }
 
