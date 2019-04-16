0de062b1e4a8e666af7b8d5b744ce414da107719
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-16 21:55:14 +0900

    Add Marker class

---
+++ src/editor/marker/Marker.ts
@@ -0,0 +1,5 @@
+type JudgeCondition = "clean" | "suspect" | "hold";
+
+export class Marker {
+  constructor(public anchorKey: string, public start: number, public end: number, public text: string, public condition: JudgeCondition){}
+}
