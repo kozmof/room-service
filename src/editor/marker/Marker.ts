390eed851713f2515bf35b93ea1a03c2a8934dc3
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-17 18:51:20 +0900

    Update

--- src/editor/marker/Marker.ts
+++ src/editor/marker/Marker.ts
@@ -1,5 +1,9 @@
 type JudgeCondition = "clean" | "suspect" | "hold";
 
 export class Marker {
-  constructor(public anchorKey: string, public start: number, public end: number, public text: string, public condition: JudgeCondition){}
+  constructor(public anchorKey: string, 
+              public start: number, 
+              public end: number, 
+              public text: string, 
+              public condition: JudgeCondition){}
 }
