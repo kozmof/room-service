b2a75de703843386c95d253c13339e5cee0a5d01
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-18 20:23:14 +0900

    Add add and remove

--- src/filing/Filing.ts
+++ src/filing/Filing.ts
@@ -8,6 +8,10 @@ class EntityData {
   constructor(
     private entity_id: DataID
   ) {}
+
+  id = () : DataID => {
+    return this.entity_id 
+  }
 }
 
 class BranchData {
@@ -18,6 +22,37 @@ class BranchData {
     private entity_array: Array<EntityData>,
     private branch_array: Array<BranchData>
   ) {}
+
+  id = () : DataID => {
+    return this.branch_id 
+  }
+
+  add_entity = (en: EntityData) => {
+    this.entity_array.push(en)
+  }
+
+  remove_entity = (entity_id: DataID) => {
+    for (let n = 0; n < this.entity_array.length; n++) {
+      if (this.entity_array[n].id() === entity_id) {
+        this.entity_array.slice(n, 1); 
+        return 
+      }
+    }
+  }
+
+  add_branch = (br: BranchData) => {
+    this.branch_array.push(br) 
+  }
+
+  remove_branch = (branch_id: DataID) => {
+    for (let n = 0; n < this.branch_array.length; n++) {
+      if(this.branch_array[n].id() === branch_id) {
+        this.branch_id.slice(n, 1); 
+        return
+      } 
+    } 
+  }
+
 }
 
 
