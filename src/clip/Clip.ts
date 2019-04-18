34ba334c2a2864dbb0066b73d1d30896ed117979
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-18 19:28:53 +0900

    Update

--- src/clip/Clip.ts
+++ src/clip/Clip.ts
@@ -1,9 +1,10 @@
+import { DataID } from "../common/CommonType"
 import { Marker } from "../editor/marker/Marker"
 
 // Block level clipping 
 export class ClipData {
-  constructor(public clip_id: DataID,
-              public clip_data: string
-              public marker_data: Marker
+  constructor(private clip_id: DataID,
+              private clip_data: string,
+              private marker_data?: Marker
               ) {}
 }
