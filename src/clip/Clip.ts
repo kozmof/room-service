5ec44dc4c452c21d142192ad08db121f46bc080f
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-18 20:31:40 +0900

    Update

--- src/clip/Clip.ts
+++ src/clip/Clip.ts
@@ -3,9 +3,9 @@ import { Marker } from "../editor/marker/Marker"
 
 // Block level clipping 
 export class ClipData {
-  constructor(
-              private clip_id: DataID,
-              private clip_data: string,
-              private marker_data?: Marker
+  constructor (
+                private clip_id: DataID,
+                private clip_data: string,
+                private marker_data?: Marker
               ) {}
 }
