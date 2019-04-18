b1bd86deae860586aa1a517699f1659882a991cb
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-18 18:58:49 +0900

    Add ClipData type

--- src/common/CommonType.ts
+++ src/common/CommonType.ts
@@ -1,10 +1,11 @@
-import { TopicEntity, BuiltInTopic, CustomTopic } from "../topic/Topic"
-import { SubjectData, ObjectData, LinkData } from "../medium_arrow/MediumArrow"
+import { TopicEntity, BuiltInTopic, CustomTopic } from "../topic/Topic";
+import { SubjectData, ObjectData, LinkData } from "../medium_arrow/MediumArrow";
+import { ClipData } from "../clip/Clip";
 
 export type DataID = string;
 export type PersonID = string;
 
-export type Data = SubjectData | ObjectData | LinkData | unknown;
+export type Data = SubjectData | ObjectData | LinkData | ClipData | unknown;
 export type DataType = "subject" | "object" | "link5x5" | "link7x7";
 
 export type UserAction = "created" | "modified" | "deleted";
