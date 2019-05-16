f1b85357d6f5f03507d413a9b49729951ba9a746
Kozo Oeda <kozo.oeda@gmail.com>
2019-05-16 22:08:24 +0900

    Clean up

--- src/common/CommonType.ts
+++ src/common/CommonType.ts
@@ -1,38 +1,13 @@
-import { TopicEntity, BuiltInTopic, CustomTopic } from "../topic/Topic";
-import { SubjectData, ObjectData, LinkData } from "../medium_arrow/MediumArrow";
-import { ClipData } from "../clip/Clip";
-
+// TODO
+export type Data = any;
 export type DataID = string;
 export type PersonID = string;
 
-export type Data = SubjectData | ObjectData | LinkData | ClipData | unknown;
-export type DataType = "subject" | "object" | "link5x5" | "link7x7";
+export type SpecificDataType = "people" | "place" | "text" | "photo" | "voice" | "event" | "incident" | "account" | "map" | "product" | "organization" | "history" | "auto" | "program" 
+export type GeneralDataType = "file";
+
+export type PatternDataType = "clip" | "topic" | "link";
+
+export type DataType = SpecificDataType | GeneralDataType | PatternDataType;
 
 export type UserAction = "created" | "modified" | "deleted";
-
-export type ModifiedDataType = string;
-
-export interface CoreDataSurface {
-  dataID: DataID;
-  source: string;
-  authorName: string;
-  authorID: PersonID;
-  editorName: string;
-  editorID: PersonID;
-  MD5: string;
-}
-
-export interface LogDataSurface extends CoreDataSurface {
-  userAction: UserAction;
-  modifiedData: ModifiedDataType;
-  time: Date;
-}
-
-export interface CommonDataSurface<T> extends CoreDataSurface {
-  builtInTopics: Array<TopicEntity<BuiltInTopic>>
-  customTopics: Array<TopicEntity<CustomTopic>>
-  history: Array<LogDataSurface>;
-  category: Array<string>;
-  tag: Array<string>
-}
-
