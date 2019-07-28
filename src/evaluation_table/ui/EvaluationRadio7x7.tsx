e1e5d4e0f2ef65b9561e1517aae4776509f32598
Kozo Oeda <kozo.oeda@gmail.com>
2019-07-28 18:13:08 +0900

    Change appearances

--- src/evaluation_table/ui/EvaluationRadio7x7.tsx
+++ src/evaluation_table/ui/EvaluationRadio7x7.tsx
@@ -70,19 +70,19 @@ export const EvaluationRadio7x7 = React.memo((props: EvaluationRadio7x7props) =>
           </Typography>
         </FormLabel>
         <RadioGroup value={props.evaluationArg.rank.sourceRank} onChange={props.onChangeSourceRank}>
-          <FormControlLabel value="N" control={<Radio disableRipple={true} />} label={provideContent("N", [data_N_0_en])}/>
+          <FormControlLabel value="N" control={<Radio disableRipple={true} />} label={provideContent("N", [data_N_0_en], data_N_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value="A" control={<Radio disableRipple={true} />} label={provideContent("A", [data_A_digest_en, data_A_0_en, data_A_1_en])}/>
+          <FormControlLabel value="A" control={<Radio disableRipple={true} />} label={provideContent("A", [data_A_0_en, data_A_1_en], data_A_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value="B" control={<Radio disableRipple={true} />} label={provideContent("B", [data_B_digest_en, data_B_0_en, data_B_1_en])}/>
+          <FormControlLabel value="B" control={<Radio disableRipple={true} />} label={provideContent("B", [data_B_0_en, data_B_1_en], data_B_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value="C" control={<Radio disableRipple={true} />} label={provideContent("C", [data_C_digest_en, data_C_0_en, data_C_1_en])}/>
+          <FormControlLabel value="C" control={<Radio disableRipple={true} />} label={provideContent("C", [data_C_0_en, data_C_1_en], data_C_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value="D" control={<Radio disableRipple={true} />} label={provideContent("D", [data_D_digest_en, data_D_0_en, data_D_1_en])}/>
+          <FormControlLabel value="D" control={<Radio disableRipple={true} />} label={provideContent("D", [data_D_0_en, data_D_1_en], data_D_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value="E" control={<Radio disableRipple={true} />} label={provideContent("E", [data_E_digest_en, data_E_0_en, data_E_1_en])}/>
+          <FormControlLabel value="E" control={<Radio disableRipple={true} />} label={provideContent("E", [data_E_0_en, data_E_1_en], data_E_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value="F" control={<Radio disableRipple={true} />} label={provideContent("F", [data_F_0_en])}/>
+          <FormControlLabel value="F" control={<Radio disableRipple={true} />} label={provideContent("F", [data_F_0_en], data_F_digest_en)}/>
         </RadioGroup>
       </FormControl>
       <FormControl component="fieldset">
@@ -92,19 +92,19 @@ export const EvaluationRadio7x7 = React.memo((props: EvaluationRadio7x7props) =>
           </Typography>
         </FormLabel>
         <RadioGroup value={props.evaluationArg.rank.infoRank.toString()} onChange={props.onChangeInfoRank}>
-          <FormControlLabel value={String(0)} control={<Radio disableRipple={true} />} label={provideContent(String(0), [data_0_0_en])}/>
+          <FormControlLabel value={String(0)} control={<Radio disableRipple={true} />} label={provideContent(String(0), [data_0_0_en], data_0_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value={String(1)} control={<Radio disableRipple={true} />} label={provideContent(String(1), [data_1_digest_en, data_1_0_en, data_1_1_en, data_1_2_en])}/>
+          <FormControlLabel value={String(1)} control={<Radio disableRipple={true} />} label={provideContent(String(1), [data_1_0_en, data_1_1_en, data_1_2_en], data_0_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value={String(2)} control={<Radio disableRipple={true} />} label={provideContent(String(2), [data_2_digest_en, data_2_0_en, data_2_1_en, data_2_2_en])}/>
+          <FormControlLabel value={String(2)} control={<Radio disableRipple={true} />} label={provideContent(String(2), [data_2_0_en, data_2_1_en, data_2_2_en], data_1_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value={String(3)} control={<Radio disableRipple={true} />} label={provideContent(String(3), [data_3_digest_en, data_3_0_en, data_3_1_en, data_3_2_en])}/>
+          <FormControlLabel value={String(3)} control={<Radio disableRipple={true} />} label={provideContent(String(3), [data_3_0_en, data_3_1_en, data_3_2_en], data_2_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value={String(4)} control={<Radio disableRipple={true} />} label={provideContent(String(4), [data_4_digest_en, data_4_0_en, data_4_1_en, data_4_2_en])}/>
+          <FormControlLabel value={String(4)} control={<Radio disableRipple={true} />} label={provideContent(String(4), [data_4_0_en, data_4_1_en, data_4_2_en], data_3_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value={String(5)} control={<Radio disableRipple={true} />} label={provideContent(String(5), [data_5_digest_en, data_5_0_en, data_5_1_en, data_5_2_en])}/>
+          <FormControlLabel value={String(5)} control={<Radio disableRipple={true} />} label={provideContent(String(5), [data_5_0_en, data_5_1_en, data_5_2_en], data_4_digest_en)}/>
           <Divider variant="middle" />
-          <FormControlLabel value={String(6)} control={<Radio disableRipple={true} />} label={provideContent(String(6), [data_6_0_en])}/>
+          <FormControlLabel value={String(6)} control={<Radio disableRipple={true} />} label={provideContent(String(6), [data_6_0_en], data_6_digest_en)}/>
         </RadioGroup>
       </FormControl>
     </DialogContent>
--- src/evaluation_table/ui/RadioContentProvider.tsx
+++ src/evaluation_table/ui/RadioContentProvider.tsx
@@ -4,7 +4,7 @@ import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
 
-export const provideContent = (signature: string, descriptionList: Array<string>) => {
+export const provideContent = (signature: string, descriptionList: Array<string>, digest?: string) => {
   const useStyles = makeStyles({
     root: {
       display: 'flex',
@@ -14,6 +14,10 @@ export const provideContent = (signature: string, descriptionList: Array<string>
 
     sig: {
       width: '8px' 
+    },
+
+    digest: {
+      marginLeft: "20px"
     },
 
     ul: {
@@ -34,19 +38,45 @@ export const provideContent = (signature: string, descriptionList: Array<string>
       )
     }
   )
-
-  return(
-    <List className={classes.root}> 
-      <ListItem className={classes.sig}>
-        <Typography variant="body1">
-          {signature}
-        </Typography>
-      </ListItem>
-      <ListItem>
-        <ul className={classes.ul}>
-          {description}
-        </ul>
-      </ListItem>
-    </List>
-  )
+  
+  if(digest){
+    return(
+      <List className={classes.root}> 
+        <ListItem className={classes.sig}>
+          <Typography variant="body1">
+            {signature}
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <List>
+            <ListItem className={classes.digest}>
+              <Typography color="primary" variant="body2">
+                {digest}
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <ul className={classes.ul}>
+                {description}
+              </ul>
+            </ListItem>
+          </List>
+        </ListItem>
+      </List>
+    )
+  } else {
+    return(
+      <List className={classes.root}> 
+        <ListItem className={classes.sig}>
+          <Typography variant="body1">
+            {signature}
+          </Typography>
+        </ListItem>
+        <ListItem>
+          <ul className={classes.ul}>
+            {description}
+          </ul>
+        </ListItem>
+      </List>
+    )
+  }
 }
