0831a03bfb0c9c7e809b26e812514b4e0146a0f2
Kozo Oeda <kozo.oeda@gmail.com>
2019-09-04 19:07:31 +0900

    Add timeline css

--- src/timeline/ui/viewer.tsx
+++ src/timeline/ui/viewer.tsx
@@ -7,10 +7,46 @@ interface TimeLineVierProps<T extends TimeContent> {
 }
 
 export const TimeLineViewer = <T extends TimeContent>(props: TimeLineVierProps<T>) => {
+  const useStyles = makeStyles(
+    {
+
+      list_line: {
+        position: 'relative',
+        margin: 0,
+        paddingBottom: '1em',
+        paddingLeft: '20px',
+
+        '&:before': {
+          backgroundColor: 'black',
+          width: '2px',
+          content: '""',
+          position: 'absolute',
+          top: '0px',
+          bottom: '0px',
+          left: '-12px'
+        },
+
+        '&:first-child': {
+          '&:before': {
+            top: '14px'
+          }
+        },
+
+        '&:last-child': {
+          '&:before': {
+            height: '6px'
+          }
+        }
+      }
+    }
+  );
+
+  const classes = useStyles({});
+
   const lists = props.timeline.data.map(
     (item: TimeData<T>) => {
       return (
-        <li>
+        <li className={classes.list_line}>
           <div>
              { item.date.toDateString() }: { item.content.detail }
           </div>
