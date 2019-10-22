953e51190b7b88836326882fa88f92ac64817b39
Kozo Oeda <kozo.oeda@gmail.com>
2019-10-22 11:06:46 +0900

    Redesign

--- src/common/CommonType.ts
+++ src/common/CommonType.ts
@@ -2,13 +2,15 @@
 export type DataID = string;
 export type PersonID = string;
 
-export type SpecificDataType = "people" | "place" | "text" | "photo" | "voice" | "event" | "incident" | "account" | "map" | "product" | "organization" | "history" | "auto" | "program";
+export type SpecificDataType = "people" | "place" | "text" |
+                               "photo" | "voice" | "event" |
+                               "incident" | "account" | "map" |
+                               "product" | "organization" | "history"; 
+
 export type GeneralDataType = "file";
 
 export type PatternDataType = "clip" | "topic" | "link";
 
 export type DataType = SpecificDataType | GeneralDataType | PatternDataType;
-
-export type UserAction = "created" | "modified" | "deleted";
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
--- src/data/Data.ts
+++ src/data/Data.ts
@@ -1,22 +1,25 @@
-import { EvaluationType } from '../evaluation_chip/table/EvaluationTableBase';
-import { EvaluationTable } from '../evaluation_chip/table/EvaluationTable';
-import { PersonID, DataID, DataType, UserAction } from '../common/CommonType';
+import { PersonID, DataID, DataType } from '../common/CommonType';
 
 export type Data<T extends DataType> = {
   readonly id: DataID;
-  readonly content: T
-  readonly sha256: string;
+  readonly content: T;
+  readonly sha256: string; // Hash of Content and Holders
 } & HolderData
 
+export type Assembly = {
+  readonly id: DataID;
+  readonly data: Map<DataID, Data<DataType>>;
+} & Editor
+
 type Person = {
-  name: string,
-  id: PersonID
+  name: string;
+  id: PersonID;
 }
 
 type Author = Person;
 type Editor = Person;
 
 type HolderData = {
-  readonly authors: Array<Author>;
-  readonly editors: Array<Editor>;
+  readonly authors: Map<PersonID, Author>;
+  readonly editors: Map<PersonID, Editor>;
 }
